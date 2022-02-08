7579624131bc6fabf201311222c77b0394ca6b1d
Jesse Jonas <jessejonas13@gmail.com>
2022-02-08 09:19:09 -0300

    style: updated themes

--- styles/theme.ts
+++ styles/theme.ts
@@ -3,30 +3,31 @@ export type DefaultTheme = typeof dark;
 const spacing = {
   xxs: 4,
   xs: 8,
-  small: 12,
-  normal: 16,
-  large: 24,
+  sm: 12,
+  md: 16,
+  lg: 24,
   xl: 32,
   xxl: 40,
-  margin: 30,
 };
 
 const sizes = {
   xxs: "0.625rem",
   xs: "0.75rem",
   sm: "0.875rem",
-  normal: "1rem",
-  large: "1.125rem",
+  md: "1rem",
+  lg: "1.125rem",
   xl: "1.5rem",
   xxl: "2rem",
-  margin: "1.875rem",
 };
 
-const breakpoints = {
-  header: 1180,
-  desktopAndUp: 1024,
-  tabletAndUp: 728,
-  mobileOnly: 500,
+const widths = {
+  xxs: 0,
+  xs: 300,
+  sm: 600,
+  md: 900,
+  lg: 1200,
+  xl: 1366,
+  xxl: 1536,
 };
 
 const light = {
@@ -37,17 +38,23 @@ const light = {
     grey300: "#7A7786",
     grey200: "#C6C5CE",
     grey100: "#F1F0F5",
-    white: "#FFFFFF",
-    blue: "#0073CE",
-    blueLight: "#00A5F0",
-    greenLight: "#00D7D7",
-    yellow: "#FFC800",
-    yellowLight: "#FFDC5C",
+
+    bg: "#FFFFFF",
+    text: "#33303E",
+
+    blue600: "#0073CE",
+    blue500: "#00A5F0",
+
+    green400: "#00D7D7",
+
+    yellow600: "#FFC800",
+    yellow500: "#FFDC5C",
+
     gradient: "linear-gradient(99.31deg, #00A5F0 7.94%, #00D7D7 95.32%)",
   },
   spacing,
   sizes,
-  breakpoints,
+  widths,
 };
 
 const dark = {
@@ -58,17 +65,23 @@ const dark = {
     grey300: "#7A7786",
     grey200: "#C6C5CE",
     grey100: "#F1F0F5",
-    white: "#FFFFFF",
-    blue: "#0073CE",
-    blueLight: "#00A5F0",
-    greenLight: "#00D7D7",
-    yellow: "#FFC800",
-    yellowLight: "#FFDC5C",
+
+    bg: "#FFFFFF",
+    text: "#33303E",
+
+    blue600: "#0073CE",
+    blue500: "#00A5F0",
+
+    green400: "#00D7D7",
+
+    yellow600: "#FFC800",
+    yellow500: "#FFDC5C",
+
     gradient: "linear-gradient(99.31deg, #00A5F0 7.94%, #00D7D7 95.32%)",
   },
   spacing,
   sizes,
-  breakpoints,
+  widths,
 };
 
 export { dark, light };
