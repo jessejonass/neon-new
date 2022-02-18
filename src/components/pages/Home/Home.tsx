aa7cbf7f5a6c4903eee660ac0b1ba0309d95757b
Jesse Jonas <jessejonas13@gmail.com>
2022-02-18 09:46:48 -0300

    fix: fixed _app and Home

--- src/components/pages/Home/Home.tsx
+++ src/components/pages/Home/Home.tsx
@@ -1,5 +1,7 @@
 import { FC } from "react";
 
+import { HomeContainer } from "./styles";
+
 import Hero from "components/Hero";
 import Introduction from "components/Introduction";
 import Banners from "components/Banners";
@@ -7,12 +9,12 @@ import SocialProof from "components/SocialProof";
 
 const Home: FC = () => {
   return (
-    <>
+    <HomeContainer>
       <Hero />
       <Introduction />
       <Banners />
       <SocialProof />
-    </>
+    </HomeContainer>
   );
 };
 
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,34 +1,34 @@
-import NextApp from "next/app";
 import Head from "next/head";
-import ReactQueryProvider from "providers/ReactQuery";
-import React from "react";
-import GlobalStyles from "../../styles/global";
+import { FC } from "react";
+import { QueryClient, QueryClientProvider } from "react-query";
 import { ThemeProvider } from "styled-components";
+import { AppProps } from "next/dist/shared/lib/router/router";
 import { light } from "../../styles/theme";
+import GlobalStyles from "../../styles/global";
 
-class App extends NextApp {
-  render() {
-    const Component = this.props.Component;
-    const pageProps = this.props.pageProps;
+const App: FC<AppProps> = ({ Component, pageProps }) => {
+  const client = new QueryClient({
+    defaultOptions: {
+      queries: {
+        retry: 1,
+      },
+    },
+  });
 
-    return (
-      <>
-        <Head>
-          <meta
-            name="viewport"
-            content="width=device-width,initial-scale=1.0"
-          />
-        </Head>
+  return (
+    <>
+      <Head>
+        <meta name="viewport" content="width=device-width,initial-scale=1.0" />
+      </Head>
 
-        <ThemeProvider theme={light}>
-          <ReactQueryProvider>
-            <GlobalStyles />
-            <Component {...pageProps} />
-          </ReactQueryProvider>
-        </ThemeProvider>
-      </>
-    );
-  }
-}
+      <ThemeProvider theme={light}>
+        <QueryClientProvider client={client}>
+          <GlobalStyles />
+          <Component {...pageProps} />
+        </QueryClientProvider>
+      </ThemeProvider>
+    </>
+  );
+};
 
 export default App;
